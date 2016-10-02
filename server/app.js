2fe6360d9889d7d8a291ce1b321bf744f19dd5ae
John Arkema <john.arkema@gmail.com>
2016-10-02 14:34:17 -0500

    basic auth0 setup working

--- server/app.js
+++ server/app.js
@@ -4,16 +4,16 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 var portDecision = process.env.PORT || 4040;
+
 app.use(bodyParser.json());
 
+//index route
 var index = require('./routes/index');
 
 
-
-
-
+//choose port send respons
 app.listen(portDecision, function(){
   console.log('Im listening on ', portDecision);
 });
-
+//use public folder
 app.use(express.static('public'));
