0abec507d581d21680d083cc2ead73d09aaba03a
John Arkema <john.arkema@gmail.com>
2016-10-04 18:50:12 -0500

    loans page

---
+++ public/scripts/controllers/loansController.js
@@ -0,0 +1,14 @@
+myApp.controller('loansController', ['$scope','$http', function($scope, $http){
+  console.log('homeController');
+
+//button to add new loans
+//on new loan button click display new loan form
+//$http get all loans
+//display in dropdown menu
+//display loan selected from dropdown in editable fashion
+//button to save  edit
+//button to delete current loan
+
+
+
+  }]);
