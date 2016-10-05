72c4ade6988d23937dfa0a171b6973cb2e21f8e0
John Arkema <john.arkema@gmail.com>
2016-10-05 11:43:37 -0500

    comments

--- public/scripts/controllers/investmentsController.js
+++ public/scripts/controllers/investmentsController.js
@@ -1,40 +1,47 @@
 myApp.controller('investmentsController',['$scope', '$http', function($scope,$http){
   console.log('investmentsController');
+
+//declare allInvestments var global
 var allInvestments;
+
+//hide all forms on page load
 $scope.addNewForm = true;
 $scope.editForm = true;
 
+//Getting all investments
 $scope.getInvest = function() {
-console.log('in get invest');
+  console.log('in get invest');
 
-  $scope.allInvestments = [];
+    //declare global var as array
+    $scope.allInvestments = [];
 
-  $http({
-    method: 'GET',
-    url: '/investments'
-  }).then(function(response){
-    // console.log('WORK WORK WORK', response);
-    $scope.allInvestments = response.data;
-    // console.log('WORK AGAIN',$scope.allInvestments);
+    //http call to get investments
+    $http({
+      method: 'GET',
+      url: '/investments'
+        }).then(function(response){
+          // console.log('WORK WORK WORK', response);
+          $scope.allInvestments = response.data;
+          // console.log('WORK AGAIN',$scope.allInvestments);
   });//end then
 };//end get.invest
+
 
 //ng option select
 //on select show edit form
 $scope.inv = function(selectedInv){
-console.log(selectedInv);
-console.log($scope.selectedInv);
-$scope.editForm = false;
-$scope.addNewForm = true;
+  console.log(selectedInv);
+  console.log($scope.selectedInv);
+
+    $scope.editForm = false;
+      $scope.addNewForm = true;
 };//end scope.inv
 
 //on add new show form
 //allow edit of all fields
 $scope.addNew = function(){
-$scope.addNewForm = false;
-$scope.editForm = true;
+  $scope.addNewForm = false;
+    $scope.editForm = true;
 };//end addNew scope
-
-
 
 }]);//end controller
