72a9d4f5e657a6cb6cee1c3515eee2fe213e4645
John Arkema <john.arkema@gmail.com>
2016-10-06 11:55:27 -0500

    put route works / db updates

--- server/routes/putRoute.js
+++ server/routes/putRoute.js
@@ -9,14 +9,15 @@ router.put('/updateInv', function(req,res){
   var newAmtInv = req.body.amountinvested;
   var profitLoss = req.body.profitLoss;
   var dateSold =req.body.dateSold;
-
+  var investmentid = req.body.invId;
+  console.log(investmentid, newAmtInv, profitLoss,dateSold);
 //connection to the data base
 pg.connect(connectionString, function(err,client,done){
   if (err) {
     console.log(err);
   }else {
     console.log('Connected to DB');
-    client.query('UPDATE investments SET (amountinvested,profitloss,solddate) VALUES($1, $2, $3)', [newAmtInv , profitLoss , dateSold]);
+    client.query('UPDATE investments SET amountinvested = $1, profitloss = $2, solddate = $3 WHERE investmentid = $4', [newAmtInv , profitLoss , dateSold, investmentid]);
     res.send({success: true});
   }//end else
 });//end pg connect
