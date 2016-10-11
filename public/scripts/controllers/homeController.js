d5061b8c659f8cba4dd0984d7780767fa00387df
John Arkema <john.arkema@gmail.com>
2016-10-11 13:33:52 -0500

    indent

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -4,22 +4,23 @@ myApp.controller('homeController', ['$scope','$http', '$timeout',function($scope
   //newsapi.org top news from Wall Street Journal
   var newsAPI = 'https://newsapi.org/v1/articles?source=the-wall-street-journal&sortBy=top&apiKey=c28f8197835d4d338e3bd3b0456e68cd';
   var newsArticles = [];
-  //Yahoo Finance API
+
+  //Finance API's
   var yahoo = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%20MSFT%20KO%20JNJ%20GOOG%20AAPL%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
   var yahoo1 = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D'http%3A%2F%2Fdownload.finance.yahoo.com%2Fd%2Fquotes.csv%3Fs%3DYHOO%2CGOOG%2CAAPL%2CMSFT%26f%3Dsl1d1t1c1ohgv%26e%3D.csv'%20and%20columns%3D'symbol%2Cprice%2Cdate%2Ctime%2Cchange%2Ccol1%2Chigh%2Clow%2Ccol2'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
   var driveWealth = "http://api.drivewealth.io/v1/quotes?symbols=AAPL,GOOG,MSFT,KO,JNJ,TSLA%20FORMAT=JSON HTTP/1.1";
   var stockQuotes;
 
   $scope.init =function(){
-  $http({
-    method:'GET',
-    url: newsAPI,
-  }).then(function(response){
-    console.log(response.data.articles);
-      $scope.newsArticles = response.data.articles;
-    });
-};//end init scope for news and stock quotes
-  $scope.init();
+    $http({
+      method:'GET',
+        url: newsAPI,
+      }).then(function(response){
+        console.log(response.data.articles);
+        $scope.newsArticles = response.data.articles;
+      });
+    };//end init scope for news and stock quotes
+      $scope.init();
 
 
   //data load time / hit api every.... //0.5min - 30000 //1 min - 60000 //5min - 300000 //10min - 600000
@@ -30,37 +31,36 @@ myApp.controller('homeController', ['$scope','$http', '$timeout',function($scope
   loadPromise;
   //get stock quotes from yahoo
   var getData = function (){
-  $http({
-    method: 'GET',
-    url: yahoo
-  }).then(function(response){
-    console.log(response);
-    $scope.stockQuotes = response.data.query.results.quote;
-
-    console.log($scope.stockQuotes, 'AFTER THE SWITCH @@@@@@');
-
-    errorCount = 0;
-    nextLoad();
-  }).catch(function(response){
-    $scope.stockQuotes = 'Server Error';
-    nextLoad(++errorCount * 2 * loadTime);
-  });
-};
-  var cancelNextLoad = function(){
-    $timeout.cancel(loadPromise);
-  };
-  var nextLoad = function(mill){
-    mill = mill || loadTime;
-    cancelNextLoad();
-    $timeout(getData, mill);
-  };
+    $http({
+      method: 'GET',
+        url: yahoo
+          }).then(function(response){
+            console.log(response);
+              $scope.stockQuotes = response.data.query.results.quote;
+                console.log($scope.stockQuotes, 'AFTER THE SWITCH @@@@@@');
+                  errorCount = 0;
+                    nextLoad();
+                  }).catch(function(response){
+                      $scope.stockQuotes = 'Server Error';
+                        nextLoad(++errorCount * 2 * loadTime);
+                      });
+                    };
+        var cancelNextLoad = function(){
+          $timeout.cancel(loadPromise);
+          };
+    var nextLoad = function(mill){
+      mill = mill || loadTime;
+        cancelNextLoad();
+          $timeout(getData, mill);
+        };
 
   //start getting gata
   getData();
   //clears the timeout, no mem leak
   $scope.$on('$destroy', function() {
       cancelNextLoad();
-    });
+      });
+
 
     $scope.stockQuotes = 'Loading...';
 }]);//end controller
--- public/scripts/controllers/loansController.js
+++ public/scripts/controllers/loansController.js
@@ -1,15 +1,6 @@
 myApp.controller('loansController', ['$scope','$http', function($scope, $http){
   console.log('homeController');
 
-
-//on new loan button click display new loan form
-
-
-//display loan selected from dropdown in editable fashion
-//button to save  edit
-//button to delete current loan
-
-
 //declare all loans var global
 var allLoans;
 var allLoanPayments;
@@ -23,103 +14,105 @@ $scope.init = function(){
   console.log('loans init');
     //make it an array
       $scope.allLoans = [];
+
 //get all loans from server
   $http({
     method: 'GET',
-    url:'/loans'
-  }).then(function(response){
-    console.log(response.data,'loans from server');
-    $scope.allLoans = response.data;
-  });//end then from http
+      url:'/loans'
+      }).then(function(response){
+        console.log(response.data,'loans from server');
+        $scope.allLoans = response.data;
+      });//end then from http
 
         //get loan payments
         $http({
           method: 'GET',
-          url:'/loanpayments'
-        }).then(function(response){
-            $scope.allLoanPayments = response.data;
+            url:'/loanpayments'
+            }).then(function(response){
+              $scope.allLoanPayments = response.data;
                 console.log($scope.allLoanPayments);
-        });
-};//end scope dot init
+              });
+            };//end scope dot init
 
-//ng option select
-//on select show edit form
-$scope.loan = function(selectedLoan){
-  console.log($scope.selectedLoan);
-    $scope.editLoanForm = false;
-      $scope.newLoanForm = true;
-        $scope.loanPayments = false;
-};//end scope.inv
+  //ng option select
+  //on select show edit form
+  $scope.loan = function(selectedLoan){
+    console.log($scope.selectedLoan);
+      $scope.editLoanForm = false;
+        $scope.newLoanForm = true;
+          $scope.loanPayments = false;
+  };//end scope.inv
 
-$scope.addNewLoan = function(){
-  $scope.newLoanForm = false;
-  $scope.editLoanForm = true;
-  $scope.loanPayments = true;
-  $scope.selectedLoan = "";
-};//end addNew scope
+  $scope.addNewLoan = function(){
+    $scope.newLoanForm = false;
+    $scope.editLoanForm = true;
+    $scope.loanPayments = true;
+    $scope.selectedLoan = "";
+  };//end addNew scope
 
 $scope.saveNewLoan = function(){
   console.log('save new loan');
 
-var newLoanToSend = {
-  fromwho: $scope.loanFromWho,
-  amount:$scope.loanAmount,
-  duration:$scope.loanDuration,
-  interestrate: $scope.loanInterestrate,
-  monthlyPay: $scope.loanMonthlyPayment,
-  notes:$scope.loanNotes
-};//end object to send
+    var newLoanToSend = {
+      fromwho: $scope.loanFromWho,
+      amount:$scope.loanAmount,
+      duration:$scope.loanDuration,
+      interestrate: $scope.loanInterestrate,
+      monthlyPay: $scope.loanMonthlyPayment,
+      notes:$scope.loanNotes
+    };//end object to send
 
-  $http({
-    method:'POST',
-    url: '/saveNewLoan',
-    data: newLoanToSend
-  }).then(function(response){
-    console.log(response);
-  });//end then on HTTP
+        $http({
+          method:'POST',
+            url: '/saveNewLoan',
+              data: newLoanToSend
+              }).then(function(response){
+                console.log(response);
+              });//end then on HTTP
 
-  //reload page on click
-  location.reload();
+        //reload page on click
+        location.reload();
 };//save new loan to the DB
 
 $scope.saveEditChanges = function(){
 console.log('save Edit Changes');
 
-var updatesToSend = {
-  rate: $scope.newRate,
-  duration: $scope.newDuration,
-  monthly: $scope.newMonthly,
-  notes: $scope.newNotes,
-  loanid: $scope.selectedLoan.loanid
-};
-  console.log(updatesToSend);
-  $http({
-    method: 'PUT',
-    url:'/updateLoan',
-    data: updatesToSend
-  }).then(function(response){
-    console.log(response,'edits have been saved for this loan');
-  });//end http then
-    location.reload();
+    var updatesToSend = {
+      rate: $scope.newRate,
+      duration: $scope.newDuration,
+      monthly: $scope.newMonthly,
+      notes: $scope.newNotes,
+      loanid: $scope.selectedLoan.loanid
+    };
+          console.log(updatesToSend);
+          $http({
+            method: 'PUT',
+              url:'/updateLoan',
+                  data: updatesToSend
+                  }).then(function(response){
+                    console.log(response,'edits have been saved for this loan');
+                  });//end http then
+            location.reload();
 };//end save edit changes loans
 
   $scope.makePayments = function(){
     console.log('making payments');
-    var paymentDataToSend = {
-      date:$scope.paymentDate,
-      amount: $scope.paymentAmt,
-      loanid: $scope.selectedLoan.loanid
-    };//end object to send
 
-    $http({
-      method: 'POST',
-      url: '/savePayment',
-      data: paymentDataToSend
-    }).then(function(response){
-      console.log(response);
-    });//end then
-      location.reload();
-  };//end make payments
+      var paymentDataToSend = {
+        date:$scope.paymentDate,
+        amount: $scope.paymentAmt,
+        loanid: $scope.selectedLoan.loanid
+      };//end object to send
+
+          $http({
+            method: 'POST',
+              url: '/savePayment',
+                data: paymentDataToSend
+                }).then(function(response){
+                  console.log(response);
+                });//end then
+            location.reload();
+        };//end make payments
 
 $scope.init();
 }]);
