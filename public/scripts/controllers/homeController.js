7391d5821aebd92b9056d3d18f5b9d0a8bb53f47
John Arkema <john.arkema@gmail.com>
2016-10-12 13:20:14 -0500

    stock ticker

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -5,7 +5,7 @@ myApp.controller('homeController', ['$scope','$http', '$timeout',function($scope
   var newsAPI = 'https://newsapi.org/v1/articles?source=the-wall-street-journal&sortBy=top&apiKey=c28f8197835d4d338e3bd3b0456e68cd';
   var newsArticles = [];
 
-  //Finance API's
+  //Finance API's 
   var yahoo = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%20MSFT%20KO%20JNJ%20GOOG%20AAPL%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
   var yahoo1 = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D'http%3A%2F%2Fdownload.finance.yahoo.com%2Fd%2Fquotes.csv%3Fs%3DYHOO%2CGOOG%2CAAPL%2CMSFT%26f%3Dsl1d1t1c1ohgv%26e%3D.csv'%20and%20columns%3D'symbol%2Cprice%2Cdate%2Ctime%2Cchange%2Ccol1%2Chigh%2Clow%2Ccol2'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
   var driveWealth = "http://api.drivewealth.io/v1/quotes?symbols=AAPL,GOOG,MSFT,KO,JNJ,TSLA%20FORMAT=JSON HTTP/1.1";
@@ -37,6 +37,7 @@ myApp.controller('homeController', ['$scope','$http', '$timeout',function($scope
           }).then(function(response){
             console.log(response);
               $scope.stockQuotes = response.data.query.results.quote;
+                // $scope.stockQuotes
                 console.log($scope.stockQuotes, 'AFTER THE SWITCH @@@@@@');
                   errorCount = 0;
                     nextLoad();
