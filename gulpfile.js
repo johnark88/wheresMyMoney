0f778ee7b4aa40fae1eac682d0cef8c97e77cb52
John Arkema <john.arkema@gmail.com>
2016-10-22 14:21:12 -0500

    gulp file update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,43 @@
-// grab our gulp packages
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+/* eslint-disable */
 
-// create a default task and just log a message
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!');
+
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var  mocha = require('gulp-mocha');
+var  qunit = require('./index');
+
+var paths = {
+    scripts: ['./*.js', '!./gulpfile.js']
+};
+
+gulp.task('lint', function () {
+    return gulp.src(paths.scripts)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
+
+gulp.task('test', function() {
+    return gulp.src('./test/*.js')
+        .pipe(mocha());
+});
+
+gulp.task('qunit:pass', function() {
+    return gulp.src('./test/fixtures/passing.html')
+        .pipe(qunit());
+});
+
+gulp.task('qunit:fail', function() {
+    return gulp.src('./test/fixtures/failing.html')
+        .pipe(qunit())
+        .on('error', function (err) {
+            console.log(err.toString());
+            this.emit('end');
+        });
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.scripts, ['lint', 'test']);
+});
+
+gulp.task('default', ['lint', 'test', 'watch']);
